54657a7696f521acd68a515e46c690fb6a9a8e11
Niiadu <jonasadjintettey@gmail.com>
2024-08-12 08:18:16 +0000

    locals file

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
-locals {
-  # ECS Fargate
-  private_subnet_ids = slice(split(",", (data.aws_ssm_parameter.private_subnets.value)), 0, 2)
-  public_subnet_ids  = split(",", (data.aws_ssm_parameter.public_subnets.value))
-}
+# locals {
+#   # ECS Fargate
+#   private_subnet_ids = slice(split(",", (data.aws_ssm_parameter.private_subnets.value)), 0, 2)
+#   public_subnet_ids  = split(",", (data.aws_ssm_parameter.public_subnets.value))
+# }
 
